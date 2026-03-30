c5e66d851a8d4bafb6fdc811afb3e0cc6d5d415f
proshehab <shehab.shakib@gmail.com>
2026-03-30 18:02:54 +0600

    core update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,25 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('authLoin');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/', [HomeController::class, 'index']);
+
+    Route::post('/search', [HomeController::class, 'search']);
+
+    Route::post('/book', [BookingController::class, 'store']);
+
+    Route::get('/dashboard', [UserController::class, 'dashboard']);
+});
+
+
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    Route::resource('buses', BusController::class);
+    Route::resource('routes', RouteController::class);
+    Route::resource('schedules', ScheduleController::class);
+});
